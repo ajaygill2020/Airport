10d22c8a47fb034d33c2734c0d243fba1f10430a
ajaygill2020 <ajaypalsgill@gmail.com>
2023-06-27 21:48:54 -0400

    Added rating images and did more testing

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import { globalStyles } from "../styles/global";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { globalStyles, images } from "../styles/global";
+import Card from "../shared/card";
 
 export default function ReviewDetails({ navigation }) {
-  const pressHandler = () => {
-    navigation.goBack();
-  };
+  const rating = navigation.getParam("rating");
 
   return (
     <View style={globalStyles.container}>
-      <Text>{navigation.getParam("title")}</Text>
-      <Text>{navigation.getParam("body")}</Text>
-      <Text>{navigation.getParam("rating")}</Text>
+      <Card>
+        <Text>{navigation.getParam("title")}</Text>
+        <Text>{navigation.getParam("body")}</Text>
+        <View style={style.rating}>
+          <Text>Flight Rating: </Text>
+          <Image
+            source={images.rating[rating]}
+            style={{
+              height: 45,
+              width: 215,
+            }}
+          />
+        </View>
+        {/* <Text>{navigation.getParam("rating")}</Text> */}
+      </Card>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "red",
+  },
+});
